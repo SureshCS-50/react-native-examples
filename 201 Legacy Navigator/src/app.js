a70fde25c1e1b062c698f0bda5b822dd98da0ded
Christoph Jerolimov <jerolimov+git+2016@gmail.com>
2016-03-29 18:45:05 +0200

    Add Navigator example

---
+++ 201 Legacy Navigator/src/app.js
@@ -0,0 +1,10 @@
+
+import React, { Component, View, Text, Image, TouchableOpacity, Navigator, StyleSheet } from 'react-native';
+
+import NavigationView from './features/navigation/NavigationView';
+
+export default class App extends Component {
+	render() {
+		return <NavigationView />;
+	}
+}
