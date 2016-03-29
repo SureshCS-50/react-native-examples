da1f3640349894862f4578d71edf2d07a7087ccf
Christoph Jerolimov <jerolimov+git+2016@gmail.com>
2016-03-29 18:56:29 +0200

    Add NavigatorIOS example

---
+++ 202 Legacy NavigatorIOS/src/app.js
@@ -0,0 +1,10 @@
+
+import React, { Component } from 'react-native';
+
+import NavigationView from './features/navigation/NavigationView';
+
+export default class App extends Component {
+	render() {
+		return <NavigationView />;
+	}
+}
